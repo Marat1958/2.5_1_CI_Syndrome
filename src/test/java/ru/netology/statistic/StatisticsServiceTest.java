d0f8e0d3e895fceaa141936091dc582a55bc8b18
Marat Safiyulin <marat.safiyulin@mail.ru>
2022-05-21 10:01:49 +0300

    Added tests fixes

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -22,50 +22,5 @@ public class StatisticsServiceTest {
         long actual = service.findMax(incomesInBillions);
         Assertions.assertEquals(expected, actual);
     }
-
-    @Test
-    void findLastMaxIncome() {
-        StatisticsService service = new StatisticsService();
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 11;
-        long actual = service.findLastMaxIncome(incomesInBillions);
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void findLastMinIncome() {
-        StatisticsService service = new StatisticsService();
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 6;
-        long actual = service.findLastMinIncome(incomesInBillions);
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void findMin() {
-        StatisticsService service = new StatisticsService();
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 3;
-        long actual = service.findMin(incomesInBillions);
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void findSum() {
-        StatisticsService service = new StatisticsService();
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 85;
-        long actual = service.findSum(incomesInBillions);
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void findAverage() {
-        StatisticsService service = new StatisticsService();
-        double[] incomesInBillions = {12.0, 5.0, 8.0, 4.0, 5.0, 3.0, 8.0, 6.0, 11.0, 11.0, 12.0};
-        double expected = (12.0 + 5.0 + 8.0 + 4.0 + 5.0 + 3.0 + 8.0 + 6.0 + 11.0 + 11.0 + 12.0) / 11.0;
-        double actual = service.findAverage(incomesInBillions);
-        Assertions.assertEquals(expected, actual);
-    }
 }
 
